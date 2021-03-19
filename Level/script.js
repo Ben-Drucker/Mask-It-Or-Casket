5520bfe989b28d166cd9de8375fed511d99778e9
Mari <mquiroz1@swarthmore.edu>
2021-03-18 22:50:19 -0400

    wip

---
+++ Level/script.js
@@ -0,0 +1,16 @@
+//const startingMinutes = 10;
+//let time = startingMinutes * 60;
+
+//const timer = document.getElementById('timer');
+
+
+/*setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEL.innerHTML = '${minutes}: ${secomds}';
+    time--;
+}
+*/
