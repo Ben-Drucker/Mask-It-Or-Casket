93921d4db9f4c628ad8a2c2785f1c24697459180
Hyeyun Chae <hchae1@swarthmore.edu>
2021-03-18 23:51:27 -0400

    testing timer

--- Level/script.js
+++ Level/script.js
@@ -14,3 +14,44 @@ function updateCountdown() {
     time--;
 }
 */
+
+/*
+var myfunc = setInterval(function () {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+
+    // Calculating minutes and seconds left
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    // Result is output to the specific element
+    document.getElementById("mins").innerHTML = minutes + " : "
+    document.getElementById("secs").innerHTML = seconds
+
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "TIME UP!!";
+    }
+}, 1000);
+*/
+
+window.onload = function () {
+    var min = 10;
+    var sec = 00;
+    var countDownTimer = setInterval(function () {
+        document.getElementById("timer").innerHTML = min + " : " + sec;
+        sec--;
+        if (sec == 00) {
+            min--;
+            sec = 60;
+            if (min == 0) {
+                clearInterval(countDownTimer)
+                document.getElementById("end").innerHTML = "TIME'S UP!";
+            }
+        }
+    }, 1000);
+}
