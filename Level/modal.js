5a14da8fe586d5e777aa6263415f1fafe9423e96
Mari <mquiroz1@swarthmore.edu>
2021-03-29 15:35:17 -0400

    sprite

--- Level/modal.js
+++ Level/modal.js
@@ -1,21 +1,38 @@
+var bar = new ProgressBar.Line(container, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'},
+    from: {color: '#FFEA82'},
+    to: {color: '#ED6A5A'},
+    step: (state, bar) => {
+      bar.path.setAttribute('stroke', state.color);
+    }
+  });
+  
+  bar.animate(1.0);
+
 // get modal
-var modal = document.getElementById("myModal");
+//var modal = document.getElementById("myModal");
 
 // get button to open
-var btn = document.getElementById("myBtn");
+//var btn = document.getElementById("myBtn");
 
 //get close button
-var close = document.getElementsByClassName("close")[0];
+//var close = document.getElementsByClassName("close")[0];
 
 //open modal when click
-btn.onclick = function() {
-    modal.style.display="block";
-}
+//btn.onclick = function() {
+//    modal.style.display="block";
+//}
 
 //close
-close.onclick = function() {
-    modal.style.display="none";
-}
+//close.onclick = function() {
+//    modal.style.display="none";
+//}
 
 /*
 document.getElementsByClassName('button').addEventListener('click', function(){
