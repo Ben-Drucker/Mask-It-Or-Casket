e7d49e928258c426f9adf118d08215f5558e7c09
Hyeyun Chae <hchae1@swarthmore.edu>
2021-04-05 23:34:29 -0400

    timer works

--- Level/timer.js
+++ Level/timer.js
@@ -4,7 +4,7 @@ var theGame = new Game(75000, 75);
 window.onload = function () {
     let interIteratoryTime = 0.5 //time between iterations, in seconds.
     var min = 1;
-    var sec = 10; //BUG Cannot be zero -> leads to bug
+    var sec = 10; 
     theGame.iterateByTime(theGame.city, interIteratoryTime, (60*min + sec)/interIteratoryTime);
     var countDownTimer = setInterval(function () {
         document.getElementById("timer").innerHTML = min + " : " + sec;
@@ -19,7 +19,6 @@ window.onload = function () {
         }
         /* 
         To make the timer countdown 10, 09, 08, 07, ..., 01, 00 seconds
-        This may not show 00 //BUG!
         */
         else if (sec < 10) {
             sec = "0" + sec;
