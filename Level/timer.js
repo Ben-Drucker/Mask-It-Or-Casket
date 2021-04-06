6760e432fc06397e640df9188a16a46b4eb59da2
Hyeyun Chae <hchae1@swarthmore.edu>
2021-04-05 23:18:52 -0400

    testing timer game over

--- Level/timer.js
+++ Level/timer.js
@@ -9,19 +9,13 @@ window.onload = function () {
     var countDownTimer = setInterval(function () {
         document.getElementById("timer").innerHTML = min + " : " + sec;
         sec--;
-        if (min < 0) {
-            clearInterval(countDownTimer);
+        if (sec == 1 && min == 0) {
             document.getElementById("end").innerHTML = "GAME OVER!";
+            clearInterval(countDownTimer);
         }
         else if (sec == 0) {
             min--;
             sec = 59;
-            /*
-            if (min == 0) {
-                clearInterval(countDownTimer)
-                document.getElementById("end").innerHTML = "GAME OVER!";
-            }
-            */
         }
         /* 
         To make the timer countdown 10, 09, 08, 07, ..., 01, 00 seconds
