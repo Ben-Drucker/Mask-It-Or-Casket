05cc1acc4e9100795d8e93244453771b54d4b811
Hyeyun Chae <hchae1@swarthmore.edu>
2021-04-05 23:07:56 -0400

    testing timer game over

--- Level/timer.js
+++ Level/timer.js
@@ -9,7 +9,7 @@ window.onload = function () {
     var countDownTimer = setInterval(function () {
         document.getElementById("timer").innerHTML = min + " : " + sec;
         sec--;
-        if (sec == 0 && min < 1) {
+        if (min < 0) {
             clearInterval(countDownTimer);
             document.getElementById("end").innerHTML = "GAME OVER!";
         }
