1211382c57e41172696a7c911946865c00b62bd8
Christian Galo <cgalo1@swarthmore.edu>
2021-04-14 12:36:26 -0400

    Interface changes
    Co-authored-by: hyeyunchae <hyeyunchae@users.noreply.github.com>

--- Level/timer.js
+++ Level/timer.js
@@ -3,8 +3,8 @@ var theGame = new Game(75000, 75);
 
 window.onload = function () {
     let interIteratoryTime = 0.5 //time between iterations, in seconds.
-    var min = 3;
-    var sec = "00"; 
+    var min = 0;
+    var sec = "10"; 
     theGame.iterateByTime(theGame.city, interIteratoryTime, (60*min + parseInt(sec))/interIteratoryTime);
     var countDownTimer = setInterval(function () {
         document.getElementById("timer").innerHTML = min + " : " + sec;
