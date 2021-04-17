7cd1e9e4d1850e36ed61a433d957e3da89146e62
Christian Galo <cgalo1@swarthmore.edu>
2021-04-16 23:07:56 -0400

    Submit username and score through ajax request

--- Level/end.js
+++ Level/end.js
@@ -3,12 +3,12 @@ function gameOver(){
     if (theGame.won == true){
         document.getElementById('endModal').innerHTML = `
         <h1>You did it!</h1>
-        <form id="submitScore" action="https://www.cs.swarthmore.edu/~cgalo1/071/leaderboard.php" method="post">
-            Name: <input type="text" name="name">
-            <input id="score" type="hidden" name="score"></input>
+        <form id="submitScore">
+            Name: <input type="text" id="name">
             <input type="button" onclick="submitScore()" value="SUBMIT">
         </form>
         <h2 id="scoreDisplay"></h2>
+        <div id="submitResponse"></div>
         `;
         document.getElementById('scoreDisplay').innerHTML = "Score: " + score;
     }
@@ -16,7 +16,21 @@ function gameOver(){
         document.getElementById('endModal').innerHTML = '<h1 id="gameOver">Game Over!</h1>';
     }
 }
+// function submitScore() {
+//     document.getElementById("score").setAttribute("value",score);
+//     document.getElementById("submitScore").submit(); //
+// }
+
 function submitScore() {
-    document.getElementById("score").setAttribute("value",score);
-    document.getElementById("submitScore").submit();
-}
+    var xhttp = new XMLHttpRequest();
+    var username = document.getElementById('name').value;
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("submitScore").innerHTML = "Your Score has been submitted";
+        document.getElementById("submitResponse").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("POST", "https://www.cs.swarthmore.edu/~cgalo1/071/leaderboard.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("name="+username+"&score="+score);
+  }
