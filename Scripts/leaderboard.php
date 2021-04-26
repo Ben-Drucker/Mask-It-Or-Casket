9c20c2945ffc306e42caefeec64682310549e6c4
Christian Galo <cgalo1@swarthmore.edu>
2021-04-26 16:46:01 -0400

    Updated leaderboard backend.

--- Scripts/leaderboard.php
+++ Scripts/leaderboard.php
@@ -7,24 +7,38 @@ error_reporting(E_ALL);
 
 $db = new SQLite3('MIOC-Leaderboard.db');
 
+// $validRegEx = array("options"=>array("regexp"=>"/^[a-zA-Z0-9 \.,_-]{0,16}/"));
+// $name = filter_input(INPUT_POST, "name", FILTER_VALIDATE_REGEXP, $validRegEx);
+
 if ($_REQUEST["type"] == "insert"){
-  $stm = $db->prepare("INSERT INTO leaderboard(name, score, date) VALUES (?, ?, ?)");
-  $stm->bindParam(1, $_REQUEST["name"]);
-  $stm->bindParam(2, $_REQUEST["score"]);
-  $stm->bindParam(3, $time);
 
-  $time = date("Y-m-d H:i:s"); //YYYY-MM-DD HH:MM:SS.SSS "ISO 8601"
+   $valid = (strlen(filter_var($_REQUEST["name"], FILTER_SANITIZE_STRING))<16);
 
-  $stm->execute();
-
-  echo $_REQUEST["name"] , "<br>" , $time;
+  //validate name
+  if ($valid) {
+    $stm = $db->prepare("INSERT INTO leaderboard(name, score, date) VALUES (?, ?, ?)");
+    $stm->bindParam(1, $_REQUEST["name"]);
+    $stm->bindParam(2, $_REQUEST["score"]);
+    $stm->bindParam(3, $time);
+  
+    $time = date("Y-m-d H:i:s"); //YYYY-MM-DD HH:MM:SS.SSS "ISO 8601"
+  
+    $stm->execute();
+  
+    echo $_REQUEST["name"] , "<br>" , $time;
+  } else {
+    echo "0";
+  }
 } 
 elseif ($_REQUEST["type"] == "retrieve") {
   $res = $db->query('SELECT * FROM leaderboard ORDER BY score DESC');
+  $rank = 1;
   while ($row = $res->fetchArray()) {
-    echo "<div class=\"grid-item\">{$row[0]}</div>
+    echo "<div class=\"grid-item\">$rank. <span class=\"username\">{$row[0]}</span></div>
     <div class=\"grid-item\">{$row[1]}</div>
     <div class=\"grid-item\">{$row[2]}</div>";
+    $rank++;
   }
 }
+
 ?>
